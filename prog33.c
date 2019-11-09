06a527b93d7cad9ff0bccd7bc895b66ccc740db5
deepakkumar-it19 <53816340+deepakkumar-it19@users.noreply.github.com>
2019-11-09 14:11:39 +0530

    fourth commit

--- prog33.c
+++ prog33.c
@@ -1,12 +1,17 @@
- #include<stdio.h>
+   #include<stdio.h>
 void main()
 {
-int a[100],b,sum=0;
-scanf("%d",&b);
-for(int i=0;i<b;i++)
+float ar=0.00,c=0.00;
+float b=0.00,hm;
+int a[100],n;
+scanf("%d",&n);
+for(int i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
-sum+=a[i];
+c=c+a[i];
+b+=1.00/a[i];
 }
-printf("\n%d",sum);
+ar=c/n;
+hm=n/b;
+printf("%.2f-Am %.2f-hm",ar,hm);
 }
